5755905b410ca0f55301402825123b7cf59e065d
Thanh Thuan <buithuan480@gmail.com>
2025-12-20 09:05:29 +0700

    Thuận: Đã tạm xong Code Logic Thuật toán pha 2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/index.css';
 import Header from './components/Header';
-
 import HomePage from './pages/HomePage';
+// Sửa tên hàm import cho đúng với file trong algorithms
+import { bubbleSort } from './algorithms/bubbleSort'; 
+import { insertionSort } from './algorithms/insertionSort';
+import { mergeSort } from './algorithms/mergeSort';
 
 function App() {
+  
+
   return (
     <Router>
       <div className="app-container">
         <Header />
-
         <main style={{ padding: '40px' }}>
           <Routes>
             <Route path="/" element={<HomePage />} />
